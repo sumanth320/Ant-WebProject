2d1090de53ffd5dcebc8ca542ef09616a69fef91
ramireddygangadhar <ramireddygangadhar@gmail.com>
2017-11-28 22:45:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ant-WebProject
+
+Once you run the ant command, it will generate the jar file.
+
+Run the jar file as follows.
+# java -jar <<Path to Jar file>>/HelloWorld-20170629.jar
+
+Note: In MAC or Linuc before running java, give the execute permissions as follows.
+#chmod -R 777 <<Path to Jar file>>/HelloWorld-20170629.jar
+
---
+++ src/com/bt/contoller/SampleController.java
@@ -0,0 +1,29 @@
+package com.bt.contoller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class SampleController {
+	@RequestMapping(value = "/getJson", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "Bhaskar Reddy L");
+		js.put("Technology", "DevOps ");
+
+		return js.toString();
+
+	}
+}
